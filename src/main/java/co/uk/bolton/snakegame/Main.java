f93c8b9c5f8d6f36298f40292cd6836cb3926e59
sachinlakshitha <hks.lakshitha@gmail.com>
2021-01-03 17:31:00 +0530

    implemened key listener

--- src/main/java/co/uk/bolton/snakegame/Main.java
+++ src/main/java/co/uk/bolton/snakegame/Main.java
@@ -5,20 +5,30 @@
  */
 package co.uk.bolton.snakegame;
 
+import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
 
 /**
  *
  * @author Sachin
  */
-public class Main {
+public class Main implements KeyListener{
     private Frame frame = null;
+    private Canvas canvas = null;
+    private Graphics graph = null;
+    private BufferStrategy strategy = null;
     private int height = 600;
     private int width = 600;
     
     public Main() {
         super();
         frame = new Frame();
+        canvas = new Canvas();
     }
     
     public static void main(String[] args) {
@@ -30,10 +40,36 @@ public class Main {
         frame.setSize(width + 7, height + 27);
         frame.setResizable(false);
         frame.setLocationByPlatform(true);
+        canvas.setSize(width + 7, height + 27);
+        frame.add(canvas);
+        canvas.addKeyListener(this);
         frame.dispose();
         frame.validate();
         frame.setTitle("Snake Game");
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
+
+        canvas.setIgnoreRepaint(true);
+        canvas.setBackground(Color.WHITE);
+
+        canvas.createBufferStrategy(2);
+
+        strategy = canvas.getBufferStrategy();
+        graph = strategy.getDrawGraphics();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
