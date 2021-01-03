58ba7ec296d17a2c54955e6ef7ed33906192cf18
sachinlakshitha <hks.lakshitha@gmail.com>
2021-01-03 22:05:28 +0530

    refactor project

---
+++ src/main/java/co/uk/bolton/snakegame/utility/Constant.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.uk.bolton.snakegame.utility;
+
+/**
+ *
+ * @author Sachin
+ */
+public class Constant {
+    public final static int height = 600;
+    public final static int width = 600;
+    public final static int gameSize = 40;
+    public final static long speed = 250;
+    
+    public static final String GAME_OVER = "GAME OVER";
+    public static final String FINAL_SCORE = "FINAL SCORE";
+    public static final String SCORE = "SCORE";
+    public static final String PAUSE = "PAUSED";
+}
