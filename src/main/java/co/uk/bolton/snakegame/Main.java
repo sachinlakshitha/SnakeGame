5ded4b8a6ce32e9f02df8fdba210443340a33af4
sachinlakshitha <hks.lakshitha@gmail.com>
2021-01-03 17:21:27 +0530

    added main frame

--- src/main/java/co/uk/bolton/snakegame/Main.java
+++ src/main/java/co/uk/bolton/snakegame/Main.java
@@ -5,12 +5,35 @@
  */
 package co.uk.bolton.snakegame;
 
+import java.awt.Frame;
+
 /**
  *
  * @author Sachin
  */
 public class Main {
+    private Frame frame = null;
+    private int height = 600;
+    private int width = 600;
+    
+    public Main() {
+        super();
+        frame = new Frame();
+    }
+    
     public static void main(String[] args) {
-        
+        Main main = new Main();
+        main.init();
+    }
+    
+    public void init() {
+        frame.setSize(width + 7, height + 27);
+        frame.setResizable(false);
+        frame.setLocationByPlatform(true);
+        frame.dispose();
+        frame.validate();
+        frame.setTitle("Snake Game");
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
     }
 }
