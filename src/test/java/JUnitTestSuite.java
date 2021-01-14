c71c28b2b2017e2755a9e9172a95e3311e288289
sachinlakshitha <hks.lakshitha@gmail.com>
2021-01-15 04:52:47 +0530

    added junit test suite

---
+++ src/test/java/JUnitTestSuite.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sachin
+ */
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({TestSnakeGame.class})
+public class JUnitTestSuite {
+    @BeforeClass
+    public static void print() {
+        System.out.println("Test Snake Game");
+    }
+}
